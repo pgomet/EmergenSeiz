96dda35a39a08b8ede55dee765d74433756c86ca
Kevin Le <kl2921@columbia.edu>
2022-07-30 11:04:08 -0400

    set up flask server and index.html

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
